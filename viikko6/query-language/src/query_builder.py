f18302643e8ce30625572827ccc449a8769ea279
honjen <144600573+honjen@users.noreply.github.com>
2024-12-08 15:41:49 +0200

    Add viikko 6

---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,26 @@
+from matchers import And, PlaysIn, HasAtLeast, HasFewerThan, All, Or
+
+class QueryBuilder:
+    def __init__(self):
+        self._matcher = All()
+
+    def plays_in(self, team):
+        self._matcher = And(self._matcher, PlaysIn(team))
+        return self
+
+    def has_at_least(self, value, attr):
+        self._matcher = And(self._matcher, HasAtLeast(value, attr))
+        return self
+
+    def has_fewer_than(self, value, attr):
+        self._matcher = And(self._matcher, HasFewerThan(value, attr))
+        return self
+    
+    def one_of(self, *matchers):
+        self._matcher = Or(*matchers)
+        return self
+
+    def build(self):
+        matcher = self._matcher
+        self._matcher = All()
+        return matcher
